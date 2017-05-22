bbf10bcad95953d9b63b901cea9b4f7f4335ed93
kim sunghan <kim sunghan>
2017-05-22 19:08:27 +0900

    글쓰기 컴포넌트 만들기전

--- 게시판/board/src/app/app.module.ts
+++ 게시판/board/src/app/app.module.ts
@@ -6,15 +6,23 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
  
-import { BoardListComponent } from "app/boardfolder/board-list/board-list.component";
+import { BoardListComponent } from "./boardfolder/board-list/board-list.component";
 import { BoardItemComponent } from './boardfolder/board-list/board-item/board-item.component';
+import { BoardWriteComponent } from './boardfolder/board-write/board-write.component';
+import { BoardReadComponent } from './boardfolder/board-read/board-read.component';
+import { CommentListComponent } from './boardfolder/comment-list/comment-list.component';
+import { CommentItemComponent } from './boardfolder/comment-list/comment-item/comment-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardListComponent,
-    BoardItemComponent
+    BoardItemComponent,
+    BoardWriteComponent,
+    BoardReadComponent,
+    CommentListComponent,
+    CommentItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ 게시판/board/src/app/boardfolder/board-read/board-read.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-read',
+  templateUrl: './board-read.component.html',
+  styleUrls: ['./board-read.component.css']
+})
+export class BoardReadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
